2d18ef29b1fe71865e49164d5e2ac20d5ab5621d
Aaron <ajbrowne0622@yahoo.com>
2023-09-20 15:38:41 -0400

    created readme

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,56 +7,6 @@ const Main = (database) => {
   //console.log(db);
   const [tasqueList, setTasqueList] = useState([]);
   //let tasqueList = {};
-  useEffect(() => {
-    get(ref(db, "tasques"))
-      .then((snapshot) => {
-        if (snapshot.exists()) {
-          setTasqueList(snapshot.val());
-          //console.log(snapshot.val());
-        } else {
-          console.log("No data available");
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
-  class TasqueView {
-    constructor(tasqueObj) {
-      this.top = tasqueObj["title"];
-      this.bottom = "";
-      if (typeof tasqueObj["dueDate"] != "undefined") {
-        this.dueDate = new Date(tasqueObj["dueDate"]);
-        this.bottom += this.dueDate.toDateString();
-      }
-    }
-
-    getTop() {
-      return this.top;
-    }
-
-    getBottom() {
-      return this.bottom;
-    }
-
-    getDueDate() {
-      return this.dueDate;
-    }
-  }
-
-  const newTask = (e) => {
-    e.preventDefault();
-    const taskVal = e.target[0].value;
-    e.target[0].value = "";
-    let tasque = makeTask2(taskVal);
-    const newTasqueRef = push(ref(db, "tasques"));
-    set(newTasqueRef, {
-      title: tasque.name,
-      dueDate: tasque.dueDate.toJSON(),
-      makeDate: tasque.makeDate.valueOf(),
-    });
-  };
 
   /*for (let i = 0; i < 10; i++) {
     taskList[i] = makeTask();
